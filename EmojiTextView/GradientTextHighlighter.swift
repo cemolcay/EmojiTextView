b67c2b263ecef08040d54918739b4cc18def8165
cemolcay <ccemolcay@gmail.com>
2016-08-31 16:07:31 -0700

    Migrate to Swift 3
    Xcode 8 beta 6 migrated EmojiTextView library and EmojiTextViewExample
    targets.

--- EmojiTextView/GradientTextHighlighter.swift
+++ EmojiTextView/GradientTextHighlighter.swift
@@ -9,71 +9,73 @@
 import Foundation
 import UIKit
 
-public class GradientTextHighlighter: TextHighlighting {
+public typealias GradientTextHighlighterCompletionHandler = (_ finished: Bool) -> Void
 
-    private var displayLink: CADisplayLink?
+open class GradientTextHighlighter: TextHighlighting {
 
-    public var animationDurationPerLetter: CGFloat = 0.06
-    public var animationOverlap: CGFloat = 0.04
+  fileprivate var displayLink: CADisplayLink?
 
-    private var startTime: CFTimeInterval = 0.0
-    weak private var textView: UITextView?
-    private var range: NSRange?
-    private var completion: ((Bool) -> ())?
+  open var animationDurationPerLetter: CGFloat = 0.06
+  open var animationOverlap: CGFloat = 0.04
 
-    public func highlightRange(range: NSRange, inTextView textView: UITextView, completion: (finished: Bool) -> ()) {
-        self.textView = textView
-        self.range = range
-        self.completion = completion
+  fileprivate var startTime: CFTimeInterval = 0.0
+  weak fileprivate var textView: UITextView?
+  fileprivate var range: NSRange?
+  fileprivate var completion: ((Bool) -> Void)?
 
-        // Animation of text on iOS is in the dire state. There doesn't seem to any other way of animating text
-        // that works with the default stack of UITextView.
-        displayLink = CADisplayLink(target: self, selector: #selector(displayLinkDidFire(_:)))
-        displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-        startTime = CACurrentMediaTime()
+  open func highlightRange(_ range: NSRange, inTextView textView: UITextView, completion: @escaping (_ finished: Bool) -> ()) {
+    self.textView = textView
+    self.range = range
+    self.completion = completion
+
+    // Animation of text on iOS is in the dire state. There doesn't seem to any other way of animating text
+    // that works with the default stack of UITextView.
+    displayLink = CADisplayLink(target: self, selector: #selector(GradientTextHighlighter.displayLinkDidFire(_:)))
+    displayLink?.add(to: RunLoop.main, forMode: .defaultRunLoopMode)
+    startTime = CACurrentMediaTime()
+  }
+
+  deinit {
+    stopDisplayLink()
+  }
+
+  open func cancel() {
+    stopDisplayLink()
+  }
+
+  fileprivate func stopDisplayLink() {
+    displayLink?.remove(from: RunLoop.main, forMode: .defaultRunLoopMode)
+    displayLink = nil
+  }
+
+  @objc open func displayLinkDidFire(_ displayLink: CADisplayLink) {
+    guard let textView = textView, let range = range else {
+        return
     }
 
-    deinit {
-        stopDisplayLink()
+    for i in 0..<range.length {
+      let letterStartTime = CGFloat(i) * (animationDurationPerLetter - animationOverlap)
+      let letterEndTime = letterStartTime + animationDurationPerLetter
+      let elapsed = CGFloat(CACurrentMediaTime()) - CGFloat(startTime)
+      let progress = max(min((elapsed - letterStartTime) / (letterEndTime - letterStartTime), 1), 0)
+
+      let color = UIColor.black.interpolateColorTo(UIColor.orange, progress: CGFloat(progress))
+
+      let mutableAttributed = textView.attributedText.mutableCopy() as! NSMutableAttributedString
+      let string = mutableAttributed.string as NSString
+
+      let letterRange = NSMakeRange(range.location + i, 1)
+      let safeRange = NSIntersectionRange(letterRange, string.range)
+
+      mutableAttributed.addAttribute(NSForegroundColorAttributeName, value: color, range: safeRange)
+      let selectedRange = textView.selectedRange
+      textView.attributedText = mutableAttributed.copy() as! NSAttributedString
+      textView.selectedRange = selectedRange
+
+      if i == range.length - 1 && progress >= 1.0 {
+          stopDisplayLink()
+          completion?(true)
+      }
     }
-
-    public func cancel() {
-        stopDisplayLink()
-    }
-
-    private func stopDisplayLink() {
-        displayLink?.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-        displayLink = nil
-    }
-
-    @objc private func displayLinkDidFire(displayLink: CADisplayLink) {
-        guard let textView = textView, let range = range else {
-            return
-        }
-
-        for i in 0..<range.length {
-            let letterStartTime = CGFloat(i) * (animationDurationPerLetter - animationOverlap)
-            let letterEndTime = letterStartTime + animationDurationPerLetter
-            let elapsed = CGFloat(CACurrentMediaTime()) - CGFloat(startTime)
-            let progress = max(min((elapsed - letterStartTime) / (letterEndTime - letterStartTime), 1), 0)
-
-            let color = UIColor.blackColor().interpolateColorTo(UIColor.orangeColor(), progress: CGFloat(progress))
-
-            let mutableAttributed = textView.attributedText.mutableCopy() as! NSMutableAttributedString
-            let string = mutableAttributed.string as NSString
-
-            let letterRange = NSMakeRange(range.location + i, 1)
-            let safeRange = NSIntersectionRange(letterRange, string.range)
-
-            mutableAttributed.addAttribute(NSForegroundColorAttributeName, value: color, range: safeRange)
-            let selectedRange = textView.selectedRange
-            textView.attributedText = mutableAttributed.copy() as! NSAttributedString
-            textView.selectedRange = selectedRange
-
-            if i == range.length - 1 && progress >= 1.0 {
-                stopDisplayLink()
-                completion?(true)
-            }
-        }
-    }
+  }
 }
